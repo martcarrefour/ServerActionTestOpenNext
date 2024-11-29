e38419e63e451debc5ce484606167e61800bb339
Dmitriy Prokushev <martcarrefour@gmail.com>
2024-11-29 15:54:10 +0300

    first commit

---
+++ app/test/page.tsx
@@ -0,0 +1,10 @@
+'use client'
+
+import TestServerAction from "../TestServerAction"
+
+export default function Test(){
+	return <div>Test
+
+		<button onClick={() => TestServerAction()}>Test Button</button>
+	</div>
+}
