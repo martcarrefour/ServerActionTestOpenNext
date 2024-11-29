84d25f8a91b76fae36523f9d4c6366c80bb5d848
Dmitriy Prokushev <martcarrefour@gmail.com>
2024-11-29 16:30:54 +0300

    fix

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,10 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+
+if (!process.env.NODE_ENV) {
+	process.env.NODE_ENV = 'production'; // Или 'development', в зависимости от ветки
+  }
+const nextConfig = {
+	
+};
 
 export default nextConfig;
